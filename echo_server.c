c8f603c209b18a353540006b0cf1b42841a4bec1
Brayden <Braydenxia@gmail.com>
2017-03-28 12:22:00 -0400

    1

--- echo_server.c
+++ echo_server.c
@@ -9,6 +9,9 @@
 * Authors: Athula Balachandran <abalacha@cs.cmu.edu>,                         *
 *          Wolf Richter <wolf@cs.cmu.edu>                                     *
 *                                                                             *
+* Update: Brayden Xia <braydenxia@gmail.com>                                  *
+*         Updated to support select from multiple socket @Mar 11, 2017        *
+*                                                                             *
 *******************************************************************************/
 #include "echo_server.h"
 
@@ -22,18 +25,19 @@ int main(int argc, char* argv[])
     fd_set read_fds, master;
     struct timeval tv;
     struct DeList *clients_head, *clients_iter, *clients_end;
-
+    FILE *server_log = fopen("echo_server.log", "w");
     //Inital values
     FD_ZERO(&read_fds);
-    tv.tv_sec = 2;
-    tv.tv_usec = 500000;
+    tv.tv_sec = 120;
+    tv.tv_usec = 0;
 
-    fprintf(stdout, "----- Echo Server -----\n");
+    fprintf(server_log, "----- Echo Server -----\n");
 
     /* all networked programs must create a socket */
     if ((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1)
     {
-        fprintf(stderr, "Failed creating socket.\n");
+        fprintf(server_log, "Failed creating socket.\n");
+        fclose(server_log);
         return EXIT_FAILURE;
     }
 
@@ -56,13 +60,15 @@ int main(int argc, char* argv[])
     if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)))
     {
         close_socket(sock);
-        fprintf(stderr, "Failed binding socket.\n");
+        fprintf(server_log, "Failed binding socket.\n");
+        fclose(server_log);
         return EXIT_FAILURE;
     }
     if (listen(sock, 5))
     {
         close_socket(sock);
-        fprintf(stderr, "Error listening on socket.\n");
+        fprintf(server_log, "Error listening on socket.\n");
+        fclose(server_log);
         return EXIT_FAILURE;
     }
     //initialize clients_list, read_fds
@@ -71,7 +77,7 @@ int main(int argc, char* argv[])
     clients_head -> next = NULL;
     clients_end = clients_head;
     FD_SET(sock, &master);
-    int max_clients = 0, i;
+    int max_clients = 0;
     /* finally, loop waiting for input and then write it back */
     while (1)
     {
@@ -81,74 +87,83 @@ int main(int argc, char* argv[])
         cli_size = sizeof(cli_addr);
         //select will hold until some message recieves
         if((select_result =
-            select(max_fds + 1, &read_fds, NULL, NULL, NULL)) < 0){
+            select(max_fds + 1, &read_fds, NULL, NULL, &tv)) < 0){
+            if(select_result == 0){
+                close(sock);
+                DeListCollect(clients_head);
+                fprintf(server_log, "Selecting Timeout. Exiting...\n");
+                fclose(server_log);
+                return EXIT_SUCCESS;
+            }
             if(select_result == -1){
                 close(sock);
-                fprintf(stderr, "Error Selecting connection.\n");
                 DeListCollect(clients_head);
+                fprintf(server_log, "Error Selecting connection.\n");
+                fclose(server_log);
                 return EXIT_FAILURE;
             }
         }
         //printf( "breakpoint: end of select\n");
         // If message comes from sock: new connections
         if(FD_ISSET(sock, &read_fds)){
-            printf( "new client is connecting\n");
+            fprintf( server_log, "new client is connecting\n");
             if ((new_sock = accept(sock, (struct sockaddr *) &cli_addr,
                                         &cli_size)) == -1)
             {
                 close(sock);
-                fprintf(stderr, "Error accepting connection.\n");
+                fprintf(server_log, "Error accepting connection.\n");
+                fclose(server_log);
+                DeListDisplay(clients_head);
                 DeListCollect(clients_head);
                 return EXIT_FAILURE;
             }
             clients_end = DeListAppend(clients_end, new_sock);
             FD_SET(new_sock, &master);
-            DeListDisplay(clients_head);
+            //DeListDisplay(clients_head);
             max_clients ++;
-            CircleTestDeList(clients_head, clients_end);
+            //CircleTestDeList(clients_head, clients_end);
         }
         // If message not comes from master sock: new message
         else{
-            //printf("client is sending message\n");
             active_sock = -1;
-            i = 0;
             for(clients_iter = clients_head; clients_iter;
                 clients_iter = clients_iter -> next){
-                i ++;
-                if(i++ < 10)
-                    printf("testing socket = %d\n", clients_iter -> sock);
                 if(FD_ISSET(clients_iter->sock, &read_fds)){
                     active_sock = clients_iter -> sock;
                     readret = 0;
                     if((readret = recv(active_sock, buf, BUF_SIZE, 0)) >= 1)
                     {
+                        //fprintf(server_log, "recieving message from client with socket = %d\n", active_sock);
                         if (send(active_sock, buf, readret, 0) != readret)
                         {
                             close_socket(active_sock);
                             close_socket(sock);
-                            fprintf(stderr, "Error sending to client.\n");
                             DeListCollect(clients_head);
+                            fprintf(server_log, "Error sending to client.\n");
+                            fclose(server_log);
                             return EXIT_FAILURE;
                         }
+                        //fprintf(server_log, "sending message back to client with socket = %d\n", active_sock);
                         memset(buf, 0, BUF_SIZE);
                     }
                     if(readret == 0){
                         //the client is closing;
                         FD_CLR(active_sock, &master);
-                        max_fds = clients_end -> sock;
+                        max_fds = clients_end -> sock;                  //this is possible wrong,if reuse socket num
                         max_clients --;
                         DeleteFromDeList(&clients_head, &clients_end, clients_iter);
-                        printf("client with socket = %d disconnects\n", active_sock);
-                        DeListDisplay(clients_head);
-                        printf("max_fds = %d\n", max_fds);
+                        //DeListDisplay(clients_head);
+                        fprintf(server_log, "client with socket = %d disconnects\n", active_sock);
+                        //fprintf(server_log, "max_fds = %d\n", max_fds);
                         CircleTestDeList(clients_head, clients_end);
                     }
                     if (readret == -1)
                     {
                         close_socket(active_sock);
                         close_socket(sock);
-                        fprintf(stderr, "Error reading from client socket.\n");
                         DeListCollect(clients_head);
+                        fprintf(server_log, "Error reading from client socket.\n");
+                        fclose(server_log);
                         return EXIT_FAILURE;
                     }
 
@@ -158,5 +173,6 @@ int main(int argc, char* argv[])
    }
    close_socket(sock);
    DeListCollect(clients_head);
+   fclose(server_log);
    return EXIT_SUCCESS;
 }
